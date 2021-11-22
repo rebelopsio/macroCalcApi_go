3663cdec539f1bbeef4b11b861964f06a4b733a4
Stephen <steve@rebelops.io>
2021-11-21 19:42:03 -0800

    added cals function

--- calculations/calc.go
+++ calculations/calc.go
@@ -8,9 +8,25 @@ func BMR(Sex string, Age int, HeightInches int, Weight int) float32 {
 	switch strings.ToUpper(Sex) {
 	case "MALE":
 		BMRVal = (10 * (Weight / 2.2)) + (6.25 * (HeightInches * 2.54)) - ((5 * Age) + 5) //  Mifflin-St. Jeor formula
-		break;
 	case "FEMALE":
 		BMRVal = (10 * (Weight / 2.2)) + (6.25 * (HeightInches * 2.54)) - ((5 * Age) + 5) - 161 //  Mifflin-St. Jeor formula
 	}
 	return BMRVal
+}
+
+func Cals(BMRVal float32, Type string) float32 {
+	var calories float32
+	var multiplier float32
+	switch strings.ToUpper(Type) {
+	case "REST":
+		multiplier = 1.2
+	case "LIGHT":
+		multiplier = 1.375
+	case "MODERATE":
+		multiplier = 1.55
+	case "HARD":
+		multiplier = 1.725
+	}
+	calories = BMRVal * multiplier
+	return calories
 }
