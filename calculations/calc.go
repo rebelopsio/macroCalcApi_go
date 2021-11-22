3c9d4e9aa44b9f42d3a913e17b7f1eaca76ab6e1
Stephen <steve@rebelops.io>
2021-11-21 19:24:17 -0800

    finished BMR function

--- calculations/calc.go
+++ calculations/calc.go
@@ -2,10 +2,15 @@ package calculations
 
 import "strings"
 
-func BMR() float32 {
-	BMRVal := 0.0
+func BMR(Sex string, Age int, HeightInches int, Weight int) float32 {
+	var BMRVal float32
+	BMRVal = 0.0
 	switch strings.ToUpper(Sex) {
-
+	case "MALE":
+		BMRVal = (10 * (Weight / 2.2)) + (6.25 * (HeightInches * 2.54)) - ((5 * Age) + 5) //  Mifflin-St. Jeor formula
+		break;
+	case "FEMALE":
+		BMRVal = (10 * (Weight / 2.2)) + (6.25 * (HeightInches * 2.54)) - ((5 * Age) + 5) - 161 //  Mifflin-St. Jeor formula
 	}
 	return BMRVal
 }
